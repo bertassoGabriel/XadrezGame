b83b32fba02351e1a89447fd65e88cf629b9146e
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-05-07 05:46:18 -0400

    Criando a primeira classe do projeto

--- XadrezGame/Program.cs
+++ XadrezGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using XadrezGame.Tabuleiro;
 
 namespace XadrezGame
 {
@@ -6,7 +7,9 @@ namespace XadrezGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
         }
     }
 }
---
+++ XadrezGame/Tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XadrezGame.Tabuleiro
+{
+    class Posicao
+    {
+        public int linha;
+        public int coluna;
+
+        public Posicao()
+        {
+        }
+        
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
