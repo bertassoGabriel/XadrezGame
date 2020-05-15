a676cbbd8639badb6fab2b42957f8265b3ff85af
bertassoGabriel <gabrieljacintho868@hotmail.com>
2020-05-15 12:44:54 -0400

    Jogada especial promoção

--- XadrezGame/Xadrez/PartidaDeXadrez.cs
+++ XadrezGame/Xadrez/PartidaDeXadrez.cs
@@ -291,6 +291,21 @@ namespace XadrezGame.Xadrez
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // #JogadaEspecial Promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -309,8 +324,6 @@ namespace XadrezGame.Xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             // #JogadaEspecial En passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
